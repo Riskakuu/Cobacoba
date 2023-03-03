ec705072cd6a0367f4d903b8ced3579555540e6f
Riskakuu <126847109+Riskakuu@users.noreply.github.com>
2023-03-03 11:46:27 +0700

    Update coba.py
    hapus yang tidak perlu

--- coba.py
+++ coba.py
@@ -3,26 +3,6 @@ print(len(list))
 print(max(list))
 print(min(list))
 
-list = ["tata","ecil","ale","el"]
-list.append("ela")
-print(list)
-print(list.count("tata"))
-
-list.extend("1")
-print(list)
-
-print(list.index("ale"))
-
-
-list.insert(1,"coco")
-print(list)
-
-print(list.pop())
-print(list)
-print(list.pop(2))
-print(list)
-print(list[-1])
-print(list)
 
 
 
